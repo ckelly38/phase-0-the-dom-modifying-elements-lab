732ebe85477977e1dee2bd695a497f00f0c4d2df
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-05-23 00:23:11 -0600

    testing modifying the DOM with JavaScript which is way easier than CSS, uses document.CreateElement('type'), element.attributes = 'whatever', document.body.append(element)

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById("main").remove();
+var newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Christopher Hugh Kelly is the champion";
+document.body.append(newHeader);
